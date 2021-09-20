dc4ca2bd83f9ed2a87f81246d7ed62dc64dede40
Hyuk Jin Choi <62084630+sc62070@users.noreply.github.com>
2021-09-21 01:13:18 +0900

    Updates

---
+++ precache-manifest.0165accae3671e1ad8e55a6b6f80417a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "badc896db68ad7aabb9a4e14745da431",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "7c8ce598a7df900c579f",
+    "url": "/nwitter/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "6f619a310c6141d46a9f",
+    "url": "/nwitter/static/js/2.5c51e07f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.5c51e07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8ce598a7df900c579f",
+    "url": "/nwitter/static/js/main.a5792748.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
